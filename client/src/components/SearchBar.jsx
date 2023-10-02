c1ee16e56b228808c7d25f8aa8ccee2b9866619d
Mark Leigh David <it.markleighdavid@gmail.com>
2023-10-02 16:29:46 +0800

    search-bar-fixed

--- client/src/components/SearchBar.jsx
+++ client/src/components/SearchBar.jsx
@@ -1,53 +1,47 @@
-import { useEffect, useState } from "react"
-// import { useParams } from 'react-router-dom';
+import { useEffect, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { icons } from "../utils/Icons";
-// import useAxiosPrivate from "../hooks/useAxiosPrivate";
-// import useAuth from "../hooks/useAuth";
-import '../styles/SearchBar.css'
+import "../styles/SearchBar.css";
 
 const SearchBar = ({ data, fn }) => {
-	const [search, setSearch] = useState('');
-	const [filtered, setFiltered] = useState();
-	const [unfiltered, setUnfiltered] = useState();
+  const [search, setSearch] = useState("");
+  const [filtered, setFiltered] = useState();
+  const [unfiltered, setUnfiltered] = useState(data); // Initialize with all ordinances
 
-	const handleChange = (e) => {
-		const searchTerm = e.target.value;
-		setSearch((prev) => {
-			return {...prev, searchTerm}
-		})
-		const filteredItems = data.filter((data) =>
-    data.title.toLowerCase().includes(searchTerm.toLowerCase()) ||
-		data.number.includes(searchTerm)
+  const handleChange = (e) => {
+    const searchTerm = e.target.value;
+    setSearch(searchTerm);
+    const filteredItems = data.filter(
+      (data) =>
+        data.title.toLowerCase().includes(searchTerm.toLowerCase()) ||
+        data.number.includes(searchTerm)
     );
     setFiltered(filteredItems);
   };
 
-	useEffect(() => {
-		setUnfiltered(data);
-	},[]);
-
-	useEffect(() => {
-		// data ? fn(unfiltered) : null
-		filtered && fn(filtered)
-		console.log(unfiltered)
-	}, [ search ])
+  useEffect(() => {
+    if (search === "") {
+      fn(unfiltered);
+    } else {
+      fn(filtered);
+    }
+  }, [ search ]);
 
   return (
     <div className="Search">
-      <input 
+      <input
         type="text"
-        id='search'
-        name='search'
-        className='Search__Inputbox'
-        placeholder='Search Ordinance'
+        id="search"
+        name="search"
+        className="Search__Inputbox"
+        placeholder="Search Ordinance"
         onChange={handleChange}
       />
-			<span>
-				<FontAwesomeIcon icon={icons.search}/>
-			</span>
+      <span>
+        <FontAwesomeIcon icon={icons.search} />
+      </span>
     </div>
-  )
-}
+  );
+};
 
-export default SearchBar
+export default SearchBar;
